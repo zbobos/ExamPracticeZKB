48d98dd28be3dd8f52f359215a3625fb98986f69
zbobos <zbobos@localhost>
2016-02-17 16:35:55 -0500

    Practice for exam

---
+++ ExamPracticeZKB/edu/saintjoe/cs/zbobos/ExamPractice/ExamPractice.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.zbobos.ExamPractice;
+
+public class ExamPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello User");
+	}
+
+}
